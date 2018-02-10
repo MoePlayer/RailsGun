4c614a98402080bc9327036f169ba8592b085fda
YongXin Shi <shiyongxin@aliyun.com>
2018-02-10 17:32:48 +0800

    danmaku type

--- app/controllers/danmakus_controller.rb
+++ app/controllers/danmakus_controller.rb
@@ -1,48 +1,63 @@
 class DanmakusController < ApplicationController
   def index
-    @danmus = Danmaku.all
-
-    #debugger
-
-    render json: {
-      code: 0,
-      danmuku: @danmus}
-  end
-
-  def index2
     @danmakus = Danmaku.all
 
     #debugger
-    @items = Array['time', 'type', 'color', 'author','text']
-
-    @danmus = Array.new
-
-    @danmakus.each_with_index do |danmaku, index|
-      #debugger
-      @danmu = Hash.new
-      @items.each_with_index do |item, item_index|
-
-        @danmu[item_index] = danmaku[item]
-        if (item == 'type')
-          @danmu[item_index] = 0
-        end
-      end
-
-      @danmus[index] = @danmu
-    end
 
     render json: {
       code: 0,
-      danmaku: @danmus}
+      danmaku: @danmakus}
+  end
+
+  def index2
+    #@danmakus = Danmaku.all
+    @danmakus = danmakuv2(Danmaku.all)
+    render json: {
+      code: 0,
+      danmaku: @danmakus
+    }
   end
 
   def create
-    @danmu = Danmaku.new(params.permit(:author, :color, :player, :text, :time, :type))
+    @danmaku = Danmaku.new(params.permit(:author, :color, :player, :text, :time, :type))
     #debugger
     #@danmu = Danmuku.new(params[:post])
     #@danmu.danmu_type = params[:type]
-    @danmu.save
-    render json: @danmu
-
+    @danmaku.save
+    render json: {
+      code: 0,
+      data: @danmaku
+    }
   end
+
+  def danmakuv2(danmakus)
+
+    #debugger
+    items = Array['time', 'type', 'color', 'author','text']
+
+    typeMap = Hash[
+      "right" => 0,
+      "top" => 1,
+      "bottom" => 2
+    ]
+
+    danmus = Array.new
+
+    danmakus.each_with_index do |danmaku, index|
+      #debugger
+      dan = Hash.new
+      items.each_with_index do |item, item_index|
+
+        dan[item_index] = danmaku[item]
+        if (item == 'type')
+          dan[item_index] = typeMap[danmaku[item]]
+        end
+      end
+
+      danmus[index] = dan
+    end
+
+    return danmus
+  end
+
 end
