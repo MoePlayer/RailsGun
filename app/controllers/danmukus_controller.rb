c0c377f9ed665b1fec3c91ee155d8581d88e053b
YongXin Shi <shiyongxin@aliyun.com>
2018-02-10 12:42:54 +0800

    get v2 danmaku

--- app/controllers/danmukus_controller.rb
+++ app/controllers/danmukus_controller.rb
@@ -4,7 +4,36 @@ class DanmukusController < ApplicationController
 
     #debugger
 
-    render json: @danmus
+    render json: {
+      code: 0,
+      danmuku: @danmus}
+  end
+
+  def index2
+    @danmukus = Danmuku.all
+
+    #debugger
+    @items = Array['time', 'type', 'color', 'author','text']
+
+    @danmus = Array.new
+    @danmu = Hash.new
+
+    @danmukus.each_with_index do |danmuku, index|
+    #debugger
+      @items.each_with_index do |item, item_index|
+
+        @danmu[item_index] = danmuku[item]
+        if (item == 'type')
+          @danmu[item_index] = 0
+        end
+      end
+
+      @danmus[index] = @danmu
+    end
+
+    render json: {
+      code: 0,
+      danmaku: @danmus}
   end
 
   def create
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'danmukus/v2', to: 'danmukus#index2'
+  post 'danmukus/v2', to: 'danmukus#create'
   resources :danmukus
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
