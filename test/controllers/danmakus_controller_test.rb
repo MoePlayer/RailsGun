237a887f3ba8cf8e4761f026a9e727cb40b7217e
metal A-wing <shiyongxin@aliyun.com>
2018-03-02 20:31:40 +0800

    Add TestUnit

--- test/controllers/danmakus_controller_test.rb
+++ test/controllers/danmakus_controller_test.rb
@@ -4,4 +4,35 @@ class DanmakusControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+  #
+  test "can create an danmaku" do
+    post "/dplayer/v2/",
+      params: { author: "awing", color:	"#fff", player: "demo", text: "fffffff", time: 0, type:	"right"}
+    assert_response :success
+    result = JSON.parse(response.body)
+    #result['code'].should == 0
+    #puts result['code']
+    #puts result
+  end
+
+  test "can read an danmaku" do
+    post "/dplayer/v2/",
+      params: { author: "awing", color:	"#fff", player: "demo", text: "fffffff", time: 0, type:	"right"}
+    assert_response :success
+
+    get "/dplayer/v2/?id=demo"
+    assert_response :success
+    result = JSON.parse(response.body)
+      #.first
+    #result['code'] == 0
+    #puts result
+    #assert_select "danmaku"
+    #assert_select {"code":0,"data":{"id":980190963,"author":"awing","color":"#fff","player":"demo","text":"fffffff","ip":"127.0.0.1","referer":null,"time":0.0,"type":"right","created_at":"2018-03-01T13:03:48.983Z","updated_at":"2018-03-01T13:03:48.983Z"}}
+  end
+
+  test "should create article" do
+    #post dplayer_v2_url, params: { id: 12 }, headers: { "HTTP_REFERER" => "http://example.com/home" }
+    #post dplayer_v2_url, params: { article: { body: 'Rails is awesome!', title: 'Hello Rails' }
+    #puts @response
+  end
 end
